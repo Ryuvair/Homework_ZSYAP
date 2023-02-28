169e5d9cdc3ae16d7bf8ed63821aaee23b2fabc0
unknown <samir-grashin@mail.ru>
2023-02-28 21:10:24 +0300

    Solved Задача N2

--- Praktika_1_Dz1/Zadacha1/Program.cs
+++ Praktika_1_Dz1/Zadacha1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int arg1, int arg2)
+{
+    int max = arg1;
+    if (arg1 == arg2)
+    {
+        Console.WriteLine("Числа равны");
+    }
+    if (arg2 > arg1)
+    {
+         max = arg2;
+    }
+    Console.Write("Макчимальное число = ");
+    Console.WriteLine(max);
+    return max;
+}
+
+int a = new Random().Next(1,1000);
+
+int b = new Random().Next(1,1000);
+
+Max(a,b);
