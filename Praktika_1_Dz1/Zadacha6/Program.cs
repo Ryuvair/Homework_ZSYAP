ce8f1199b49ddf763f7a51de0b3d635ecd6133cb
unknown <samir-grashin@mail.ru>
2023-02-28 22:56:30 +0300

    Solved Задача N6

---
+++ Praktika_1_Dz1/Zadacha6/Program.cs
@@ -0,0 +1,23 @@
+﻿int even(int arg)
+{
+int remainder = 0;
+remainder = arg % 2;
+     if (remainder != 0) 
+     {
+        Console.Write("Число нечетное ! ") ;
+     }
+     else
+     {
+        Console.Write("Число четное ! ");
+     }
+      Console.Write ("Число = " );
+        Console.WriteLine ( arg ) ;
+     return arg;
+
+
+} 
+
+int a = new Random().Next(1,1000);
+
+even(a);
+
