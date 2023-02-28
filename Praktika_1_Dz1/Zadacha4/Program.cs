335e0e1de33743a375c41a947e32e6f43d7ee4f9
unknown <samir-grashin@mail.ru>
2023-02-28 21:30:47 +0300

    Solved Задача N4

--- Praktika_1_Dz1/Zadacha4/Program.cs
+++ Praktika_1_Dz1/Zadacha4/Program.cs
@@ -1,7 +1,7 @@
-﻿int Max(int arg1, int arg2)
+﻿int Max(int arg1, int arg2, int arg3)
 {
     int max = arg1;
-    if (arg1 == arg2)
+    if ( arg1 == arg2 & arg3 == arg2 )
     {
         Console.WriteLine("Числа равны");
     }
@@ -9,21 +9,28 @@
     {
          max = arg2;
     }
+    if (arg3 > max)
+    {
+        max = arg3;
+    }
     Console.Write("Макcимальное число = ");
     Console.WriteLine(max);
     return max;
 }
-void Print(int arg1, int arg2)
-
+void Print(int arg1, int arg2, int arg3)
 {
     Console.Write("Первое число = ");
     Console.WriteLine(arg1);
      Console.Write("Второе число = ");
     Console.WriteLine(arg2);
+     Console.Write("Третие число = ");
+    Console.WriteLine(arg3);
 }
-
 int a = new Random().Next(1,1000);
+
 int b = new Random().Next(1,1000);
 
-Print(a,b);
-Max(a,b);
+int c = new Random().Next(1,1000);
+
+Print(a,b,c);
+Max(a,b,c);
