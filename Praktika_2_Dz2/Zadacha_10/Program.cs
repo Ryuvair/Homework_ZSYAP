33d28292fb32d5c1afc7654f6b7d1c35c326eb6c
unknown <samir-grashin@mail.ru>
2023-03-02 20:11:58 +0300

    HomeWork 2 Zadacha 10

---
+++ Praktika_2_Dz2/Zadacha_10/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Ведите трехзначное число число : ");
+
+int nubmer = int.Parse(Console.ReadLine());
+
+ while ( nubmer  < 100 || nubmer > 1000) 
+{
+    Console.WriteLine("Вы ввели не коректное число введите заного");
+    Console.Write("Ведите трехзначное число  : ");
+    nubmer = int.Parse(Console.ReadLine());
+     
+}
+
+
+
+int Digit(int arg)
+{
+
+   
+
+int FirstDigit = arg / 100;
+
+int SecondDigit = arg / 10 - FirstDigit * 10;
+
+int TherdDigit = arg - FirstDigit * 100  - SecondDigit * 10;
+
+Console.Write("Первое число : ");
+Console.WriteLine(FirstDigit);
+
+Console.Write("Второе число : ");
+Console.WriteLine(SecondDigit);
+
+Console.Write("Третье число : ");
+Console.WriteLine(TherdDigit);
+
+return 0;
+
+    }
+Digit(nubmer);
