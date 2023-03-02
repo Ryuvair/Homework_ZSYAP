ed78862d637306618738f22632e345fdbcb099eb
unknown <samir-grashin@mail.ru>
2023-03-02 21:10:17 +0300

    Zadacha13

---
+++ Praktika_2_Dz2/Zadacha13/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int FindTherdDigit(int arg)
+{
+while (arg > 1000)
+{     
+    arg = arg / 10;
+}
+
+if (arg < 100 )
+{
+    Console.WriteLine("Третьего числа нет"); 
+}
+
+else
+{
+    int FirstDigit = arg / 100;
+    int SecondDigit = arg / 10 - FirstDigit * 10;
+    int TherdDigit = arg - FirstDigit * 100  - SecondDigit * 10;
+
+    Console.Write("Третье число : ");
+    Console.WriteLine(TherdDigit);
+}
+
+return 0;
+
+}
+Console.Write("Введите число : ");
+int nubmer = int.Parse(Console.ReadLine());
+
+
+
+
+FindTherdDigit(nubmer);
