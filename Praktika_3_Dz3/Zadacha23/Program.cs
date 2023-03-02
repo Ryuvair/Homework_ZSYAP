b142087aa24a009d7c643ccbc6782aa409877cf7
unknown <samir-grashin@mail.ru>
2023-03-03 00:23:51 +0300

    Zadacha23

---
+++ Praktika_3_Dz3/Zadacha23/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int SearchForCube(int arg)
+{
+    double ans = 0;
+
+    for (int i = 1; i <= arg; i++)
+    {
+        ans = Math.Pow(i,3);
+        Console.Write(i);
+        Console.Write(" В кубе : ");
+        Console.WriteLine (ans);
+    }
+return 0 ;
+}
+
+Console.Write("Введите число : ");
+int N = int.Parse(Console.ReadLine());
+
+SearchForCube(N);
