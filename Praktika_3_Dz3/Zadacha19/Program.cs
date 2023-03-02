2ba3b03843ddb264797170a2386311dbe2e4e4ed
unknown <samir-grashin@mail.ru>
2023-03-02 23:42:19 +0300

    Zadacha19

---
+++ Praktika_3_Dz3/Zadacha19/Program.cs
@@ -0,0 +1,50 @@
+﻿
+int PalindromeFiveDigitCheck(int arg)
+{
+    while ( arg  <= 10000 || arg >= 100000) 
+{
+    Console.WriteLine("Вы ввели не коректное число введите заного");
+    Console.Write("Ведите пятизначное число  : ");
+    arg = int.Parse(Console.ReadLine());
+    //arg = fivedigit;     
+}
+
+    int FirstDigit = arg / 10000;
+    int SecondDigit = arg / 1000 - FirstDigit * 10;
+    int FivthDigit = arg % 10;
+    int FourthDigit = (arg % 100 - FivthDigit) / 10;
+
+    Console.Write("Первое число : ");
+    Console.WriteLine(FirstDigit);
+
+    Console.Write("Второе число : ");
+    Console.WriteLine(SecondDigit);
+
+    Console.Write("Четвертое число : ");
+    Console.WriteLine(FourthDigit);
+
+    Console.Write("Пятое число : ");
+    Console.WriteLine(FivthDigit);
+
+  
+    if (FirstDigit == FivthDigit & FourthDigit == FourthDigit)
+    {
+        Console.Write("Число : ");
+        Console.WriteLine(arg);
+        Console.Write("Является палиндромом");
+
+    }
+    else
+    {
+        Console.Write("Число : ");
+        Console.WriteLine(arg);
+        Console.WriteLine("Не Является палиндромом");
+    }
+    return 0;
+}
+
+
+Console.Write("Веддите пятизначное число : ");
+int fivedigit = int.Parse(Console.ReadLine());
+
+PalindromeFiveDigitCheck(fivedigit);
