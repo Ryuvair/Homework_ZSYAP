cca8ce08d8fe40df9019d2c9374424979bb9bf94
unknown <samir-grashin@mail.ru>
2023-03-12 18:53:35 +0300

    Zadacha29

---
+++ Praktika_4_Dz4/Zadacha_29/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] GenerateArray(int Length, int Max, int Min)
+{       
+    
+    int[] array = new int [Length]; 
+    Random random = new Random();
+    for (int i = 0; i < Length; i++)
+{
+        array[i] = random.Next(Min, Max + 1);
+}
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length ; i++)
+    {
+        //Console.Write("№");
+        //Console.Write($"{i+1}:");
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write("]");
+}
+Console.WriteLine("Введдите максимальное значение диапазона случаных чисел которые заполнят массив");
+    int max = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введдите минимальное значение диапазона случаных чисел которые заполнят массив");
+    int min = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите колличество элементов массива");
+    int length = int.Parse(Console.ReadLine());
+int[] array = GenerateArray(length,max,min);
+PrintArray(array);
