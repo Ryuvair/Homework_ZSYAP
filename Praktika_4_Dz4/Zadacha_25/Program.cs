e00fab54842b0ebe866ff55c1e8973626657ee46
unknown <samir-grashin@mail.ru>
2023-03-12 17:15:48 +0300

    Zadacha25

---
+++ Praktika_4_Dz4/Zadacha_25/Program.cs
@@ -0,0 +1,19 @@
+﻿void DegreeOfNumber(double arg1, double arg2)                // Первыый аргумент должен присваивать значения числа которое хотим возвести в степень
+{                                                    // Второе аргумент должен присваивать значение степени
+    double ans = Math. Pow(arg1, arg2);
+
+    Console.WriteLine("Число : ") ;
+    Console.Write(arg1);
+    Console.Write(" В степени : ") ;
+    Console.Write(arg2);
+    Console.Write(" = ") ;
+    Console.Write(ans);
+}
+
+Console.WriteLine("Введите число которое хотите возвести в степень");
+double A = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите степень числа");
+double B = double.Parse(Console.ReadLine());
+
+DegreeOfNumber(A,B);
